f14effdcef4e0336f19a30ee94b3c254ca331b6a
Niko Kovacevic <nikovacevic@gmail.com>
2018-09-11 17:48:14 -0600

    Rewrite of logger

--- internal/pkg/log/log.go
+++ internal/pkg/log/log.go
@@ -5,13 +5,18 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 const (
+	// INFO is used for basic information
 	INFO = iota
+	// DEBUG is used for debugging purposes
 	DEBUG
+	// WARNING is used to report warnings
 	WARNING
+	// ERROR is used to log errors
 	ERROR
 )
 
@@ -27,14 +32,49 @@ type Logger struct {
 	log.Logger
 }
 
-// NewLogger creates a new Logger
-func NewLogger(out io.Writer) *Logger {
+// New creates a new Logger
+func New(out io.Writer) *Logger {
 	return &Logger{
 		Logger: *log.New(out, "", 0),
 	}
 }
 
-// LogRequests wraps an http.Handler with per-request logging to stdout
+// Fatal is equivalent to Log, followed by a call to os.Exit(1)
+func (l *Logger) Fatal(priority int, format string, v ...interface{}) {
+	s := fmt.Sprintf(
+		"%-29s %-8s %s",
+		time.Now().Format("2006-01-02 15:04:05"),
+		prefix[priority],
+		fmt.Sprintf(format, v...),
+	)
+	l.Print(s)
+	os.Exit(1)
+}
+
+// Log prints the given formatting string and spread parameters in the manner
+// of Printf to Logger, l, prepended with timestamp and priority
+func (l *Logger) Log(priority int, format string, v ...interface{}) {
+	l.Printf(
+		"%-29s %-8s %s",
+		time.Now().Format("2006-01-02 15:04:05"),
+		prefix[priority],
+		fmt.Sprintf(format, v...),
+	)
+}
+
+// Panic is equivalent to Log, followed by a panic
+func (l *Logger) Panic(priority int, format string, v ...interface{}) {
+	s := fmt.Sprintf(
+		"%-29s %-8s %s",
+		time.Now().Format("2006-01-02 15:04:05"),
+		prefix[priority],
+		fmt.Sprintf(format, v...),
+	)
+	l.Print(s)
+	panic(s)
+}
+
+// LogRequests wraps an http.Handler with per-request logging
 func (l *Logger) LogRequests(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
@@ -47,14 +87,4 @@ func (l *Logger) LogRequests(next http.Handler) http.Handler {
 			r.RequestURI,
 		)
 	})
-}
-
-// Log wraps an http.Handler with per-request logging to stdout
-func (l *Logger) Log(priority int, format string, v ...interface{}) {
-	l.Printf(
-		"%-29s %-8s %s",
-		time.Now().Format("2006-01-02 15:04:05"),
-		prefix[priority],
-		fmt.Sprintf(format, v...),
-	)
 }
