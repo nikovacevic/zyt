73d119dcb5f593089fe813e611567ee643dcd194
Niko Kovacevic <nikovacevic@gmail.com>
2018-09-11 17:49:30 -0600

    Update main cmd to work with recent pkg changes

--- cmd/zyt/main.go
+++ cmd/zyt/main.go
@@ -1,34 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/nikovacevic/zyt/internal/pkg/err"
 	"github.com/nikovacevic/zyt/internal/pkg/http"
-	"github.com/nikovacevic/zyt/internal/pkg/jwt"
 	"github.com/nikovacevic/zyt/internal/pkg/log"
 	"github.com/nikovacevic/zyt/internal/pkg/postgres"
 )
 
 func main() {
-	logger := log.NewLogger(os.Stdout)
-	errorService := err.NewService(logger)
+	logger := log.New(os.Stdout)
+
+	// Configure session
+	// TODO
+	// TODO pass to AuthService
 
 	// Configure and connect to Postgres
-	pgConfig, err := postgres.GetConfig("../..")
-	errorService.CheckAndFatal(err)
-	db, err := postgres.NewDB(*pgConfig)
-	errorService.CheckAndFatal(err)
-
-	// Configure JWT
-	jwtConfig, err := jwt.GetConfig("../..")
-	errorService.CheckAndFatal(err)
-	jwtService := jwt.NewService(*jwtConfig)
+	pgConfig, err := postgres.GetConfig("config", "postgres")
+	if err != nil {
+		logger.Fatal(log.ERROR, fmt.Sprintf("Postgres config: %s", err.Error()))
+	}
+	pgdb, err := postgres.NewDB(*pgConfig)
+	if err != nil {
+		logger.Fatal(log.ERROR, err.Error())
+	}
 
 	// Connect services to server and start listening
 	server := http.NewServer()
-	http.NewAuthController(db, errorService, jwtService, logger).Route(server)
-	http.NewEventController(db, logger).Route(server)
-	http.NewUserController(db, logger).Route(server)
+	// http.NewAuthController(pgdb, errorService, jwtService, logger).Route(server)
+	http.NewEventController(pgdb, logger).Route(server)
+	http.NewUserController(pgdb, logger).Route(server)
+
 	http.ListenAndServe(":1234", logger.LogRequests(server))
 }
